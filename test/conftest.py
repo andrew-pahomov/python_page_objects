a2cc6c5b40ba005b69252e7af19284f08d21e09c
andrew-pahomov <andrew-pahomov@yandex.ru>
2023-08-13 12:41:18 +0300

    добавил опции

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,10 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture(scope="session")
 def driver():
     options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1420,1080')
     options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(service=Service(), options=options)
     yield driver
     driver.quit()
